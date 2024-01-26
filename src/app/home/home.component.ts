c910064a1cdd41773c9dd1717d4c9be120413f2e
thomas <t.armand@groupe-aen.info>
2024-01-26 22:02:01 +0100

    Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  showModal = false;
+  dragover = false;
+
+  dragOverHandler(event: DragEvent) {
+    event.preventDefault();
+    this.dragover = true;
+  }
+
+  dragLeaveHandler() {
+    this.dragover = false;
+  }
+
+  onFileDrop(event: DragEvent) {
+    event.preventDefault();
+    this.dragover = false;
+  }
+
+  openModal() {
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
 }
+
+ 
