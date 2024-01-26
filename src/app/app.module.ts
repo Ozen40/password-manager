599aca4c005a5d214d80fe146ed487956b9f0cc8
enzo.chauvet <enzo.chauvet@sfr.fr>
2024-01-26 16:32:41 +0100

    Ajout de home et manage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ManageComponent } from './manage/manage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ManageComponent
   ],
   imports: [
     BrowserModule,
