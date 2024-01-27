df26c6c6fd4e573c772c28829573ebfb6675947f
enzo.chauvet <enzo.chauvet@sfr.fr>
2024-01-27 21:00:47 +0100

    Dev Valentin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { DisplayComponent } from './display/display.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HomeComponent, DisplayComponent]
+  imports: [RouterOutlet, HomeComponent, DisplayComponent, FormComponent]
 })
 export class AppComponent {
   title = 'password-manager';
