6457ead49cf7256e948922ff6ee9eb5a6ebe8c50
enzo.chauvet <enzo.chauvet@sfr.fr>
2024-01-28 22:54:12 +0100

    Enregistrement JSON dans local storage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm } from '@angular/forms';
 import { EncryptionService } from '../service/encryptionService';
-import { ActivatedRoute, Router} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 
 @Component({
@@ -31,8 +31,8 @@ export class HomeComponent {
   hidePassword: boolean = true;
   hideConfirmPassword: boolean = true;
 
-  constructor(private encryptionService: EncryptionService,  private route: ActivatedRoute,
-    private router: Router) {}
+  constructor(private encryptionService: EncryptionService, private route: ActivatedRoute,
+    private router: Router) { }
 
 
   validatePassword(): boolean {
@@ -115,7 +115,7 @@ export class HomeComponent {
     }
   }
 
-  
+
 
   sendFile(): void {
     console.log('Envoi du fichier:', this.fileName);
@@ -129,25 +129,34 @@ export class HomeComponent {
     event.preventDefault();
     const files = event.dataTransfer?.files;
     if (files && files.length > 0) {
-      this.handleFile(files[0]);
+      const file = files[0];
+      this.handleFile(file);
     }
   }
 
   onFileSelected(event: Event): void {
     const input = event.target as HTMLInputElement;
     if (input.files && input.files.length > 0) {
-      this.handleFile(input.files[0]);
+      const file = input.files[0];
+      this.handleFile(file);
     }
   }
 
   private handleFile(file: File): void {
-    if (file.type === "application/json") {
-      this.fileName = file.name;
-      this.errorMessage = '';
-      console.log('Fichier JSON détecté:', file.name);
+    if (file && file.type === 'application/json') {
+      const reader = new FileReader();
+
+      reader.onload = (loadEvent) => {
+        const fileContent = loadEvent.target?.result as string;
+
+        localStorage.setItem('jsonFileContent', fileContent);
+        console.log(fileContent);
+        console.log('Contenu du fichier JSON enregistré dans le stockage local.');
+      };
+
+      reader.readAsText(file);
     } else {
-      this.errorMessage = 'Veuillez déposer uniquement des fichiers JSON.';
-      this.fileName = '';
+      console.error('Veuillez sélectionner un fichier JSON.');
     }
   }
 
